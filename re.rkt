af8c51f28529e11a14a87ec531e6f636d9da08d6
Dreary Lisper <drearylisper@gmail.com>
2021-11-19 15:46:44 +0000

    Support to have several states.

--- re.rkt
+++ re.rkt
@@ -1,36 +1,47 @@
 #lang racket
 
-(struct re/regexp
+(struct re/fa
   (first-state last-state) #:mutable)
 
-(struct re/state
+(struct re/fa-state
   (edges is-final?) #:mutable)
 
-(struct re/edge
-  (symbol) #:mutable)
+(define (new-fa-state)
+  (re/fa-state (make-hash) #f))
 
-(define (new-state)
-  (re/state (make-hash) #f))
+(define (add-state from-state symbol to-state)
+  (define has-to-state (hash-has-key? (re/fa-state-edges from-state) symbol))
+  (define to-states (if has-to-state
+                        (hash-ref (re/fa-state-edges from-state) symbol)
+                        (mutable-set)))
+  (set-add! to-states to-state)
+  (hash-set! (re/fa-state-edges from-state) symbol to-states))
 
-;; (define (re/or . patterns))
+(define (get-states state symbol)
+  (define has-to-state (hash-has-key? (re/fa-state-edges state) symbol))
+  (if has-to-state (hash-ref (re/fa-state-edges state)) #f))
 
-(define (re/match regexp s)
+
+(define (re/match fa s)
   (define symbols (string->list s))
   (define (match-inner state symbols)
     (cond
-     ((empty? symbols) (re/state-is-final? state))
-     ((not (hash-has-key? (re/state-edges state) (first symbols))) #f)
+     ((empty? symbols) (re/fa-state-is-final? state))
+     ((not (hash-has-key? (re/fa-state-edges state) (first symbols))) #f)
      (else
-      (define next-state (hash-ref (re/state-edges state) (first symbols)))
-      (match-inner next-state (rest symbols)))))
-  (match-inner (re/regexp-first-state regexp) symbols))
+      (define next-states (hash-ref (re/fa-state-edges state) (first symbols)))
+      (if (> (set-count next-states) 1)
+          (raise "Can't match non-determenistic finite automaton")
+          (match-inner (set-first next-states) (rest symbols))))))
+  (match-inner (re/fa-first-state fa) symbols))
 
-(define (re/string s)
+(define (re/string->fa s)
   (define symbols (string->list s))
 
+
   (define (add-symbol state symbol)
-    (define next-state (new-state))
-    (hash-set! (re/state-edges state) symbol next-state)
+    (define next-state (new-fa-state))
+    (add-state state symbol next-state)
     next-state)
 
   (define (add-symbols state symbols)
@@ -41,13 +52,19 @@
       (define next-state (add-symbol state symbol))
       (add-symbols next-state (rest symbols)))))
 
-  (define first-state (new-state))
-  (define final-state (add-symbols first-state symbols))
+  (define first-state (new-fa-state))
+  (define last-state (add-symbols first-state symbols))
 
-  (set-re/state-is-final?! final-state #t)
-  (re/regexp first-state final-state))
+  (set-re/fa-state-is-final?! last-state #t)
+  (re/fa first-state last-state))
 
+;; (define (re/or a b)
+;;   (set-re/state-is-final?! (re/regexp-last-state a) #f)
+;;   (set-re/state-is-final?! (re/regexp-last-state a) #f)
+;;   (define first-state (new-state))
+;;   (define last-state (new-state))
+;;   )
 
 (re/match
- (re/string "ABC")
+ (re/string->fa "ABC")
  "ABC")
