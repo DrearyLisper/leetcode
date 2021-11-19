1253516b7643db2abc534aa46b6580529bcc39f3
Dreary Lisper <drearylisper@gmail.com>
2021-11-19 22:22:23 +0000

    On the way of getting DFA.

--- re.rkt
+++ re.rkt
@@ -10,16 +10,16 @@
   (re/fa-state (make-hash) #f))
 
 (define (add-state from-state symbol to-state)
-  (define has-to-state (hash-has-key? (re/fa-state-edges from-state) symbol))
-  (define to-states (if has-to-state
-                        (get-states from-state symbol)
-                        (mutable-set)))
+  (define to-states (get-states from-state symbol))
   (set-add! to-states to-state)
   (hash-set! (re/fa-state-edges from-state) symbol to-states))
 
 (define (get-states state symbol)
   (define has-to-state (hash-has-key? (re/fa-state-edges state) symbol))
-  (if has-to-state (hash-ref (re/fa-state-edges state) symbol) #f))
+  (if has-to-state (hash-ref (re/fa-state-edges state) symbol) (mutable-set)))
+
+(define (get-symbols state)
+  (hash-keys (re/fa-state-edges state)))
 
 
 (define (re/match fa s)
@@ -58,13 +58,35 @@
   (set-re/fa-state-is-final?! last-state #t)
   (re/fa first-state last-state))
 
-;; (define (re/or a b)
-;;   (set-re/state-is-final?! (re/regexp-last-state a) #f)
-;;   (set-re/state-is-final?! (re/regexp-last-state a) #f)
-;;   (define first-state (new-state))
-;;   (define last-state (new-state))
-;;   )
+(define (re/or a b)
+  (set-re/fa-state-is-final?! (re/fa-last-state a) #f)
+  (set-re/fa-state-is-final?! (re/fa-last-state a) #f)
+  (define first-state (new-fa-state))
+  (define last-state (new-fa-state))
+  (add-state first-state 'eps (re/fa-first-state a))
+  (add-state first-state 'eps (re/fa-first-state b))
+  (add-state (re/fa-last-state b) 'eps last-state)
+  (add-state (re/fa-last-state b) 'eps last-state)
+  (set-re/fa-state-is-final?! last-state #t)
+  (re/fa first-state last-state))
 
-(re/match
- (re/string->fa "ABC")
- "ABC")
+(define (re/ndfa->dfa fa)
+  (define (closure state)
+    (define (closure-inner state clojure)
+      (define eps-reachable (get-states state 'eps))
+      (set-add! clojure state)
+      (for ((s (in-set eps-reachable))) (closure-inner s clojure))
+      clojure)
+    (closure-inner state (mutable-set)))
+  (define (symbols states)
+    (set-subtract (apply set-union (set-map states get-symbols)) '(eps)))
+  (define (next-states states symbol)
+    (apply mutable-set (apply set-union (set-map states (lambda (state) (set->list (get-states state symbol)))))))
+  (define first-state (closure (re/fa-first-state fa)))
+  (map (lambda (symbol) (cons symbol (next-states first-state symbol))) (symbols first-state)))
+
+
+(define our-fa (re/or (re/string->fa "A") (re/string->fa "B")))
+
+(re/match our-fa "A")
+(re/ndfa->dfa our-fa)
